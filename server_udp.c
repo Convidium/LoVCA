717e83193514b1ddf674ea8be52cc2d5d6bc6637
Convidium <denyssianchuk0@gmail.com>
2026-08-05 00:54:17 +0300

    Rewrote server_udp.c whitout AI

--- server_udp.c
+++ server_udp.c
@@ -1,89 +1,47 @@
 #define _POSIX_C_SOURCE 200112L
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <string.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #define SERVER_PORT "8841"
 #define MAX_BUFF_LEN 100
 
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
 int main(void) {
-    int sockfd;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    int numbytes;
-    struct sockaddr_storage their_addr; // We don't know if their address is IPv4 or IPv6, so we set the structure that allows both
-    char buff[MAX_BUFF_LEN];
-    socklen_t addr_len;
-    char s[INET6_ADDRSTRLEN]; // s is an address, and we reserve biggest possible size (IPv6 > IPv4)
+    int socketfd;
+
+    int status;
+    struct addrinfo hints, *servinfo;
 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET6;
+    hints.ai_family = AF_INET;
     hints.ai_flags = AI_PASSIVE;
     hints.ai_socktype = SOCK_DGRAM;
 
-    // NULL - since we set our future socket to listen to all networks (local, wifi, ethernet, etc.)
-    if ((rv = getaddrinfo(NULL, SERVER_PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    getaddrinfo(NULL, SERVER_PORT, &hints, &servinfo);
+
+    socketfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+    if (socketfd == -1) {
+        perror("Error: something with socket()");
         return 1;
     }
 
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("listener: socket");
-            continue;
-        }
+    status = bind(socketfd, servinfo->ai_addr, servinfo->ai_addrlen);
 
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("listener: bind");
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL) {
-        fprintf(stderr, "listener: failed to bind socket\n");
+    if (status == -1) {
+        close(socketfd);
+        perror("Error: something with bind()");
         return 2;
     }
 
     freeaddrinfo(servinfo);
 
-    printf("listener: waiting to recvfrom...\n");
+    printf("Server: waiting to recvfrom...\n");
 
-    addr_len = sizeof their_addr;
-    if ((numbytes = recvfrom(sockfd, buff, MAX_BUFF_LEN-1 , 0,
-        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-        perror("recvfrom");
-        exit(1);
-    }
-
-    printf("listener: got packet from %s\n",
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s));
-    printf("listener: packet is %d bytes long\n", numbytes);
-    buff[numbytes] = '\0';
-    printf("listener: packet contains \"%s\"\n", buff);
-
-    close(sockfd);
-
+    
+        
     return 0;
 }
