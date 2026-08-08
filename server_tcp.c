8ed5372c17dae77ce45490dce8b4aebb33a2a522
Convidium <denyssianchuk0@gmail.com>
2026-08-09 00:51:27 +0300

    Rewrote TCP server in a functional way

--- server_tcp.c
+++ server_tcp.c
@@ -19,11 +19,40 @@
 #define MAX_CLIENTS 20
 #define MAX_BUFF_LEN 256
 
-int main(void) {
-    signal(SIGPIPE, SIG_IGN);
-    
+typedef enum {
+    SERVER_CONTINUE = 0,
+    SERVER_STOP = 1
+} server_control_t;
+
+
+// Convert socket to IP address string.
+// addr: struct sockaddr_in or struct sockaddr_in6
+const char *inet_ntop2(void *addr, char *buf, size_t size) {
+    struct sockaddr_storage *sas = addr;
+    struct sockaddr_in *sa4;
+    struct sockaddr_in6 *sa6;
+    void *src;
+
+    switch (sas->ss_family) {
+        case AF_INET:
+            sa4 = addr;
+            src = &(sa4->sin_addr);
+            break;
+        case AF_INET6:
+            sa6 = addr;
+            src = &(sa6->sin6_addr);
+            break;
+        default:
+            return NULL;
+    }
+    return inet_ntop(sas->ss_family, src, buf, size);
+}
+
+// Create and establish new socket (fd listener)
+int get_listener_socket(void) {
     int listen_fd;
     struct addrinfo hints, *servinfo;
+    int yes = 1;
 
     int addr_status;
     int bind_status;
@@ -36,17 +65,16 @@ int main(void) {
     addr_status = getaddrinfo(NULL, PORT, &hints, &servinfo);
     if (addr_status != 0) {
         fprintf(stderr, "Error: getaddrinfo()\n");
-        return 1;
+        exit(1);
     }
 
     listen_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
     if (listen_fd == -1) {
         perror("Error: socket()");
         freeaddrinfo(servinfo);
-        return 2;
+        exit(1);
     }
 
-    int yes = 1;
     setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
     bind_status = bind(listen_fd, servinfo->ai_addr, servinfo->ai_addrlen);
@@ -54,7 +82,7 @@ int main(void) {
         perror("Error: socket()");
         close(listen_fd);
         freeaddrinfo(servinfo);
-        return 3;
+        exit(1);
     }
 
     freeaddrinfo(servinfo);
@@ -62,7 +90,99 @@ int main(void) {
     if (listen(listen_fd, BACKLOG) == -1) {
         perror("Error: listen()");
         close(listen_fd);
-        return 4;
+        return -1;
+    }
+
+    return listen_fd;
+}
+
+void handle_incoming_connection(int listener, int client_sockets[]) {
+    struct sockaddr_storage new_addr; // Client address
+    socklen_t addrlen;
+    int newfd;  // Newly accept()'ed socket descriptor
+
+    addrlen = sizeof new_addr;
+    newfd = accept(listener, (struct sockaddr *)&new_addr, &addrlen);
+
+    if (newfd == -1) {
+        perror("Error: accept()");
+        return;
+    }
+    
+    int added = 0;
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] == -1) {
+            client_sockets[i] = newfd;
+            printf("\n{+} New client connected! (Slot %d)\n", i);
+            added = 1;
+            break;
+        }
+    }
+    if (!added) {
+        printf("\n{-} Server full! Connection rejected.\n");
+        close(newfd);
+    }
+}
+
+server_control_t handle_stdin_input(int client_sockets[]) {
+    char send_buf[MAX_BUFF_LEN];
+    if (fgets(send_buf, sizeof send_buf, stdin) == NULL) {
+        clearerr(stdin);
+        printf("\n[Server] EOF detected on stdin. Exiting server...\n");
+        return SERVER_STOP;
+    }
+
+    send_buf[strcspn(send_buf, "\n")] = '\0';
+
+    if (strcmp(send_buf, "exit") == 0) {
+        printf("Exiting server...\n");
+        return SERVER_STOP;
+    }
+
+    int len = strlen(send_buf);
+    if (len == 0) return SERVER_CONTINUE;
+
+    int sent_count = 0;
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] != -1) {
+            char msg_with_newline[MAX_BUFF_LEN + 2];
+            snprintf(msg_with_newline, sizeof(msg_with_newline), "%s\n", send_buf);
+
+            int bytes = send(client_sockets[i], msg_with_newline, strlen(msg_with_newline), 0);
+            
+            if (bytes == -1) {
+                printf("{-} Failed to send to slot %d, closing connection.\n", i);
+                close(client_sockets[i]);
+                client_sockets[i] = -1;
+            } else {
+                sent_count++;
+            }
+        }
+    }
+    printf("Broadcasted to %d clients.\n", sent_count);
+    return SERVER_CONTINUE;
+}
+
+server_control_t process_connections(struct pollfd fds[], int listen_fd, int client_sockets[]) {
+    if (fds[1].revents & POLLIN) {
+        handle_incoming_connection(listen_fd, client_sockets);
+    }
+
+    if (fds[0].revents & (POLLIN | POLLHUP)) {
+        return handle_stdin_input(client_sockets);
+    }
+
+    return SERVER_CONTINUE;
+}
+
+
+int main(void) {
+    signal(SIGPIPE, SIG_IGN);
+    
+    int listen_fd = get_listener_socket();
+    if (listen_fd == -1) {
+        fprintf(stderr, "Failed to initialize listener socket.\n");
+        return 1;
     }
 
     printf("=== Broadcast Chat Server started on port %s ===\n", PORT);
@@ -79,9 +199,8 @@ int main(void) {
     fds[1].fd = listen_fd;
     fds[1].events = POLLIN;
 
-    char send_buf[MAX_BUFF_LEN];
-
-    while (1) {
+    int running = 1;
+    while (running) {
         printf("Server > ");
         fflush(stdout);
 
@@ -91,63 +210,9 @@ int main(void) {
             break;
         }
 
-        if (fds[1].revents & POLLIN) {
-            struct sockaddr_storage client_addr;
-            socklen_t addr_len = sizeof client_addr;
-            int new_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &addr_len);
-
-            if (new_fd != -1) {
-                int added = 0;
-                for (int i = 0; i < MAX_CLIENTS; i++) {
-                    if (client_sockets[i] == -1) {
-                        client_sockets[i] = new_fd;
-                        printf("\n{+} New client connected! (Slot %d)\n", i);
-                        added = 1;
-                        break;
-                    }
-                }
-                if (!added) {
-                    printf("\n{-} Server full! Connection rejected.\n");
-                    close(new_fd);
-                }
-            }
-        }
-
-        if (fds[0].revents & (POLLIN | POLLHUP)) {
-            if (fgets(send_buf, sizeof send_buf, stdin) == NULL) {
-                clearerr(stdin);
-                printf("\n[Server] EOF detected on stdin. Exiting server...\n");
-                continue;
-            }
-
-            send_buf[strcspn(send_buf, "\n")] = '\0';
-
-            if (strcmp(send_buf, "exit") == 0) {
-                printf("Exiting server...\n");
-                break;
-            }
-
-            int len = strlen(send_buf);
-            if (len == 0) continue;
-
-            int sent_count = 0;
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (client_sockets[i] != -1) {
-                    char msg_with_newline[MAX_BUFF_LEN + 2];
-                    snprintf(msg_with_newline, sizeof(msg_with_newline), "%s\n", send_buf);
-
-                    int bytes = send(client_sockets[i], msg_with_newline, strlen(msg_with_newline), 0);
-                    
-                    if (bytes == -1) {
-                        printf("{-} Failed to send to slot %d, closing connection.\n", i);
-                        close(client_sockets[i]);
-                        client_sockets[i] = -1;
-                    } else {
-                        sent_count++;
-                    }
-                }
-            }
-            printf("Broadcasted to %d clients.\n", sent_count);
+        server_control_t status = process_connections(fds, listen_fd, client_sockets);
+        if (status == SERVER_STOP) {
+            running = 0;
         }
     }
 
