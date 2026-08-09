e2422e4ea94404b39dfb486de8f7750a1fd1ae00
Convidium <denyssianchuk0@gmail.com>
2026-08-09 12:01:59 +0300

    Rewrote client_tcp in a functional way

--- client_tcp.c
+++ client_tcp.c
@@ -16,6 +16,11 @@
 #define PORT "8841"
 #define MAX_BUFF_LEN 256
 
+typedef enum {
+    SERVER_CONTINUE = 0,
+    SERVER_STOP = 1
+} server_control_t;
+
 void *get_in_addr(struct sockaddr *sa)
 {
     if (sa->sa_family == AF_INET) {
@@ -25,47 +30,39 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[]) {
+int get_client_socket(char address[]) {
     int sockfd;
-    char buf[MAX_BUFF_LEN];
     char server_ip[INET_ADDRSTRLEN];
 
-    int addr_status, connect_status;
+    int addr_status;
     struct addrinfo hints, *servinfo;
-
-    if (argc != 2) {
-        fprintf(stderr, "Error: client hostname required\n");
-        return 1;
-    }
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
-    addr_status = getaddrinfo(argv[1], PORT, &hints, &servinfo);
+    addr_status = getaddrinfo(address, PORT, &hints, &servinfo);
 
     if (addr_status != 0) {
         fprintf(stderr, "Error: getaddrinfo: %s\n", gai_strerror(addr_status));
-        freeaddrinfo(servinfo);
-        return 2;
+        return -1;
     }
 
     sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
     if (sockfd == -1) {
         perror("Error: socket()");
         freeaddrinfo(servinfo);
-        return 3;
+        return -1;
     }
 
     inet_ntop(servinfo->ai_family, get_in_addr((struct sockaddr *)servinfo->ai_addr), server_ip, sizeof server_ip);
     printf("Client: attempting connection to %s\n", server_ip);
 
-    connect_status = connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
-    if (connect_status == -1) {
+    if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
         perror("Client: connect()");
         close(sockfd);
         freeaddrinfo(servinfo);
-        return 4;
+        return -1;
     }
 
     inet_ntop(servinfo->ai_family, get_in_addr((struct sockaddr *)servinfo->ai_addr), server_ip, sizeof server_ip);
@@ -73,7 +70,77 @@ int main(int argc, char *argv[]) {
     printf("Type 'exit' or press Ctrl+D to disconnect this server.\n\n");
 
     freeaddrinfo(servinfo);
-    
+
+    return sockfd;
+}
+
+server_control_t handle_server_message(int sockfd) {
+    char buf[MAX_BUFF_LEN];
+    int bytes_received = recv(sockfd, buf, MAX_BUFF_LEN - 1, 0);
+
+    if (bytes_received == -1) {
+        perror("Error: recv()");
+        return SERVER_STOP;
+    } else if (bytes_received == 0) {
+        printf("\nClient: Server closed connection without sending data.\n");
+        return SERVER_STOP;
+    }
+
+    buf[bytes_received] = '\0';
+    buf[strcspn(buf, "\r\n")] = '\0'; 
+    printf("Server message (%d bytes):\n\"%s\"\n", bytes_received, buf);
+
+    return SERVER_CONTINUE;
+}
+
+server_control_t handle_stdin_input() {
+    char send_buf[MAX_BUFF_LEN];
+    if (fgets(send_buf, sizeof send_buf, stdin) == NULL) {
+        // Either Ctrl + D (EOF) was pressed or some write error happened
+        printf("\nClient: closing connection...\n");
+        // Clear the flag that signalizes EOF
+        clearerr(stdin); 
+        return SERVER_STOP;
+    }
+
+    if (strcmp(send_buf, "exit\n") == 0) {
+        printf("Client: Disconnecting by operator command.\n");
+        return SERVER_STOP;
+    }
+
+    return SERVER_CONTINUE;
+}
+
+server_control_t process_connections(struct pollfd fds[], int sockfd) {
+    if (fds[1].revents & (POLLIN | POLLHUP)) {
+        if (handle_server_message(sockfd) == SERVER_STOP) {
+            return SERVER_STOP;
+        }
+    }
+
+    if (fds[0].revents & (POLLIN | POLLHUP)) {
+        if (handle_stdin_input() == SERVER_STOP) {
+            return SERVER_STOP;
+        }
+    }
+
+    return SERVER_CONTINUE;
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd;
+
+    if (argc != 2) {
+        fprintf(stderr, "Error: client hostname required\n");
+        return SERVER_STOP;
+    }
+
+    sockfd = get_client_socket(argv[1]);
+    if (sockfd == -1) {
+        fprintf(stderr, "Failed to initialize client socket.\n");
+        return SERVER_STOP;
+    }
+
     struct pollfd fds[2];
     fds[0].fd = STDIN_FILENO;
     fds[0].events = POLLIN;
@@ -81,44 +148,17 @@ int main(int argc, char *argv[]) {
     fds[1].fd = sockfd;
     fds[1].events = POLLIN;
 
-    char send_buf[MAX_BUFF_LEN];
-
-    while(1) {
+    int running = 1;
+    while(running) {
         int poll_count = poll(fds, 2, -1);
         if (poll_count == -1) {
             perror("Error: poll()");
             break;
         }
 
-        if (fds[1].revents & POLLIN) {
-            int bytes_received = recv(sockfd, buf, MAX_BUFF_LEN - 1, 0);
-
-            if (bytes_received == -1) {
-                perror("Error: recv()");
-                break;
-            } else if (bytes_received == 0) {
-                printf("\nClient: Server closed connection without sending data.\n");
-                break;
-            }
-
-            buf[bytes_received] = '\0';
-            buf[strcspn(buf, "\r\n")] = '\0'; 
-            printf("Server message (%d bytes):\n\"%s\"\n", bytes_received, buf);
-        }
-
-        if (fds[0].revents & POLLIN) {
-            if (fgets(send_buf, sizeof send_buf, stdin) == NULL) {
-                // Either Ctrl + D (EOF) was pressed or some write error happened
-                printf("\nCLient: closing connection...\n");
-                // Clear the flag that signalizes EOF
-                clearerr(stdin); 
-                break;
-            }
-
-            if (strcmp(send_buf, "exit\n") == 0) {
-                printf("Client: Disconnecting by operator command.\n");
-                break;
-            }
+        server_control_t status = process_connections(fds, sockfd);
+        if (status == SERVER_STOP) {
+            running = 0;
         }
     }
 
